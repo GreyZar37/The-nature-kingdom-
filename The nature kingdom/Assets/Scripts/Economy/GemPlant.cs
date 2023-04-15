99f9f9f6054e84eae866a9fced2f37fbd6d85cad
Stationary Smooth Stoinkle <103361572+SmoothStoinkleSunnySun@users.noreply.github.com>
2023-04-15 19:36:36 +0200

    Mere oprydning i scripts 4

--- The nature kingdom/Assets/Scripts/Economy/GemPlant.cs
+++ The nature kingdom/Assets/Scripts/Economy/GemPlant.cs
@@ -1,7 +1,5 @@
 using System.Collections;
 using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using UnityEditor.Rendering;
 using UnityEngine;
 
 public class GemPlant : MonoBehaviour
